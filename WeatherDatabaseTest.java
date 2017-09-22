10f6a84a808bb17649bdff2140b20ef74bdab58b
laerni <32196084+laerni@users.noreply.github.com>
2017-09-22 14:37:52 +0300

    Add files via upload

---
+++ WeatherDatabaseTest.java
@@ -0,0 +1,31 @@
+import org.junit.jupiter.api.Test;
+
+class WeatherDatabaseTest {
+
+    @Test
+    public void isTemperature() {
+        //
+        //assertEquals(Character.isDigit(), weather.result());
+    }
+
+    @Test
+    public void wheatherHighIsHigherThanLow() {
+        //assertEquals(, weather.result());
+    }
+
+    @Test
+    public void areCoordinatesNormal() {
+        //boolean b = Pattern.matches("***:***", weather);
+        //assertEquals("", weather.result());
+    }
+
+    @Test
+    public void isAllTheInfoIncluded() {
+        //assertEquals();
+    }
+
+    @Test
+    public void isMinimum() {
+        //assertEquals();
+    }
+}
